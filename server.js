5d4bbd79ade5eef0b0afa35aaa18c86018c8e81c
asimov86 <kennyjosue8@gmail.com>
2022-07-21 23:07:58 -0300

    Desafío API RESTful

--- server.js
+++ server.js
@@ -1,69 +1,35 @@
 const express = require("express");
 const fs = require("fs");
+const { Router } = express;
 const app = express();
+const routerProducto = Router();
+const productos = [];
 
-class Contenedor{
-    constructor(fileName){
-        this.fileName = fileName;    
-    }
-
-    async getAll(){
-        
-        try{
-            let data = await fs.promises.readFile(`./${this.fileName}.txt`, 'utf-8');
-            data = JSON.parse(data);
-            return data;
-        }catch(err){
-            return console.log('Error de lectura!', err);
-        }
-    }
-
-    async getRandom(){
-
-        try{
-            let data = await fs.promises.readFile(`./${this.fileName}.txt`, 'utf-8');
-            data = JSON.parse(data);
-            let randomPos = getRandomPos(1, data.length - 1);
-            let prodRandom = data[randomPos];
-            return prodRandom;
-
-        }catch(err){
-            return console.log('Error de lectura!', err);
-        }
-    }
-
-}
-
-function getRandomPos(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)+min);
-  }
+//Utilizar JSON en las request (Cuerpo)
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+//
 
 
-const producto = new Contenedor('productos');
-
-app.get('/', (req, res)=>{
-
-    res.send({mensaje: "Bienvenidos a la ruta raíz"});
+//Rutas para productos
+routerProducto.get('/', (req, res) => {
+    res.send(productos);
 })
 
-app.get('/productoRandom', async (req, res)=>{
-
-    const prodRandom = await producto.getRandom();
-    res.send({productoRandom: prodRandom});
+routerProducto.post('/', (req, res) => {
+    const producto = req.body;
+    productos.push(producto);
+    res.json(productos);
 })
 
-app.get('/productos', async (req, res)=>{
+app.use('/api/productos', routerProducto);
 
-    const prod = await producto.getAll();
-
-
-    res.send({productos: prod});
-})
 
 const PORT = 8080;
 
-app.listen(PORT, ()=>{
+const server = app.listen(PORT, ()=>{
     console.log(`Servidor escuchando en puerto ${PORT}`);
 })
+
+
+server.on("error", error => console.log(`Error: ${error}`));
